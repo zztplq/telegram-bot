fd33dd1c191bdddf035554bb63a11711eb28f6a9
zztplq <zztplq@icloud.com>
2023-01-21 18:46:34 +0300

    fun add
    add добавлен

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import config as cf
 from aiogram import Bot, Dispatcher, executor, types
-subs_list = [1,2,3,4,5,6,7,8,9, 10,12,11,13,14,15,16,17,28,18,19,20,21,22,23,24,25,26,27]
+subs_list = [1,2,3,4,5,6,7,8,9,10,12,11,13]
 # Configure logging
 logging.basicConfig(level = logging.INFO)
 bot = Bot(token = cf.token)
@@ -30,12 +30,30 @@ async def send_bay_command(message: types.message):
             else:
                 for i in range(args):
                     #print(i)
-                    subs_list.pop()
+                    subs_list.append()
                 return await bot.send_message(message.chat.id, f"Осталось {len(subs_list)} подписок")
         else:
             return await bot.send_message(message.chat.id, "Введите другое числовое значение, так как такого количества подписок нет")
 
 
+
+@dp.message_handler( commands = ['add'])
+async def send_welcom(message: types.message):
+    print(message.chat.id)
+    args = message.get_args()
+    if message.chat.id != 1099292405:
+        return await bot.send_message(message.chat.id, "Сейчас {len(subs_list)} подписок. Сколько вы хотите добавить?")
+    if not args:
+        return await bot.send_message(message.chat.id, f"Вы добавили подписки. Сейчас подписок {len(subs_list)} ")
+    else:
+        if args.isdigit():
+            args = int(args)
+            for i in range(args):
+                #print(i)
+                subs_list.append(i)
+            return await bot.send_message(message.chat.id, f"Вы добавили подписки. Осталось {len(subs_list)} подписок")
+        else:
+            return await bot.send_message(message.chat.id, "Вы превысили лимит, введите число меньше")
 
 
 
@@ -50,7 +68,7 @@ async def send_welcom(message: types.message):
 @dp.message_handler( commands = ['s'])
 async def send_welcom(message: types.message):
     return await bot.send_message(message.chat.id, "Найден человек, чтобы найти нового человека: /next"
-                        " чтобы выйти в главное меню: /stop")
+                        " чтобы выйти в главное меню: /stop" "Купить подписку /buy")
 
 @dp.message_handler( commands = ['stop'])
 async def send_welcom(message: types.message):
